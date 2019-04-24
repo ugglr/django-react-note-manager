f579622e7d0f748d8883d04b956219c706305901
CarlW <carl@oskarroger.net>
2019-04-24 14:06:46 +0800

    added webpack, babel and react dependencies and display front end running react with django templates.

--- notemanager/notemanager/settings.py
+++ notemanager/notemanager/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
